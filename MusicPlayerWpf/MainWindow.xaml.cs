462fa2708016ebd2e0a68c80ce153dc2bcdcdf5c
Koi Den <prostobydet@gmail.com>
2024-06-08 08:29:30 +0300

    window state example

--- MusicPlayerWpf/MainWindow.xaml.cs
+++ MusicPlayerWpf/MainWindow.xaml.cs
@@ -91,5 +91,16 @@ namespace MusicPlayerWpf
         {
             this.Close();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void MinimazieBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
